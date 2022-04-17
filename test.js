7343f6bb32bf55ca1fd5ef2b2e39bb3415ad3cec
ngoctaidhwar3 <ngoctaidhwar@gmail.com>
2022-04-17 22:31:07 +0700

    Check prime number

--- test.js
+++ test.js
@@ -1,15 +1,25 @@
-const calculator = require('./math');
+const number = 0;
+let isPrimeNumber = true;
+ 
+// Nếu number bé hơn 2 tức là không phải số nguyên tố
+if (number < 2){
+    isPrimeNumber = false;
+}
+else{
+    // lặp từ 2 tới number-1
+    for (let index = 2; index < number-1; index++)
+    {
+        if (number % index === 0){
+            isPrimeNumber = false;
+            break;
+        }
+    }
+}
 
-var x = 50, y = 10; 
-    
-console.log(`Addition of ${x} and ${y} is `
-                   + calculator.add(x, y)); 
-    
-console.log(`Subtraction of ${x} and ${y} is `
-                   + calculator.sub(x, y)); 
-    
-console.log(`Multiplication of ${x} and ${y} is `
-                   + calculator.mult(x, y)); 
-    
-console.log(`Division of ${x} and ${y} is `
-                    + calculator.div(x, y)); 
+// Kiểm tra biến isPrimeNumber
+if (isPrimeNumber === true){
+    console.log(number + " là số nguyên tố");
+}
+else{
+    console.log(number + " không phải là số nguyên tố");
+}
